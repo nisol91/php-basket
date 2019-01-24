d51def9f1555896be3af4163b961acb0960bbbdd
nisol91 <nicolasolzi91@gmail.com>
2019-01-24 14:44:46 +0100

    2 [code generato]

--- data.php
+++ data.php
@@ -1,1 +1,28 @@
-<?php var_dump('$vars...') ?>
+<?php
+
+function getCode($num) {
+
+  $str = '';
+  $str_2 = '';
+
+  for ($i=0; $i < ($num/2); $i++) {
+    $str .= rand(0, 9);
+  }
+
+
+  $letters = ['a','b','c','d','e','f','g','h','i','l','m','n','o','p','q','r','s','t','u','v','z'];
+  for ($i=0; $i < ($num/2); $i++) {
+    $lettersLength = count($letters);
+    $pos = $letters[rand(0, $lettersLength)];
+    $str_2 .= $pos;
+    $str_2_up = strtoupper($str_2);
+  }
+  return $str.$str_2_up;
+};
+
+echo getCode (6);
+
+
+
+
+?>
